90dd12811f374e75425aa0a547e3ee996d574c3e
Dharmesh53 <dhiru7321r@gmail.com>
2024-09-04 22:56:03 +0530

    verify token middleware fix

--- backend/utils/verifyToken.js
+++ backend/utils/verifyToken.js
@@ -1,26 +1,27 @@
 const jwt = require("jsonwebtoken");
 
 const verifyToken = async (req, res, next) => {
-    try {
-        const cookie = req.headers.cookie;
-        const token = cookie?.split("=")[1];
+  try {
+    const cookie = req.headers.cookie;
+    const token = cookie?.split("=")[1];
 
-        if (!token) {
-            res.status(404).json({ msg: "Token not found !!" });
-        }
-
-        jwt.verify(String(token), process.env.MY_JWT_SECERT, (err, data) => {
-            if (err) {
-                return res.status(400).json({ msg: "Invalid Token" });
-            }
-
-            req.id = data.user;
-            req.email = data.email;
-            next();
-        });
-    } catch (error) {
-        return res.status(500).json({ msg: error.message });
+    if (!token) {
+      console.log(cookie);
+      return res.status(404).json({ msg: "Token not found !!" });
     }
+
+    jwt.verify(String(token), process.env.MY_JWT_SECERT, (err, data) => {
+      if (err) {
+        return res.status(400).json({ msg: "Invalid Token" });
+      }
+
+      req.id = data.user;
+      req.email = data.email;
+      next();
+    });
+  } catch (error) {
+    return res.status(500).json({ msg: error.message });
+  }
 };
 
 module.exports = verifyToken;
